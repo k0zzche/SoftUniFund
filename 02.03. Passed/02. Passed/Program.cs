e8aee34bc9126b2b0525b261d39dc89035d9c75d
k0zzche <120604173+k0zzche@users.noreply.github.com>
2025-01-22 20:03:00 +0200

    Add files via upload

---
+++ 02.03. Passed/02. Passed/Program.cs
@@ -0,0 +1,10 @@
+﻿double n = double.Parse(Console.ReadLine());
+
+if (n >= 3.00)
+{
+    Console.WriteLine("Passed!");
+}
+else 
+{
+    Console.WriteLine("Failed!");
+}
---
+++ 04. Back in 30 min/04. Back in 30 min/Program.cs
@@ -0,0 +1,15 @@
+﻿int h = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+int n = m + 30;
+
+if (h >= 0 && h <= 23 && n >= 00 && n >= 59)
+    {
+        h++;
+        n = n - 60;
+    }
+
+   if (h > 23)
+    {
+        h = 0;
+    }
+Console.WriteLine($"{h}:{n:d2}");
